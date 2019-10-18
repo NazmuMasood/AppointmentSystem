8502b60c1cf464ba31ddb02d4e4f7cabf3f34015
NazmuMasood <44304205+NazmuMasood@users.noreply.github.com>
2019-10-19 01:50:53 +0600

    FirebaseAuth - Country Code And PhoneNo Input handled

--- app/src/main/java/com/example/appointmentsystem/SignupActivity.java
+++ app/src/main/java/com/example/appointmentsystem/SignupActivity.java
@@ -5,29 +5,54 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
 //import com.facebook.FacebookSdk;
 //import com.facebook.appevents.AppEventsLogger;
 
 public class SignupActivity extends AppCompatActivity {
-    EditText phoneNoET; Button signupButton; Spinner spinner;
+    EditText phoneNoET; Button signupButton;
+    Spinner spinner; EditText countryCodeET;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
         //Initializing content
         phoneNoET = findViewById(R.id.phoneNoET);
+
         signupButton = findViewById(R.id.signupButton);
+
+        countryCodeET = findViewById(R.id.countryCodeET);
+        countryCodeET.setFocusable(false);
+
         spinner = findViewById(R.id.countryCodeSpinner);
+
         spinner.setAdapter(new ArrayAdapter<String>(this,
                 android.R.layout.simple_spinner_dropdown_item, CountryData.countryNames));
         spinner.setSelection(13);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String countryCode = CountryData.countryAreaCodes[position];
+                countryCodeET.setText("+"+countryCode);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
         phoneNoET.setText("1521328932");
 
