178eeae559bff3a915332f90c19b09774b623506
NazmuMasood <nazmumasood96@gmail.com>
2020-09-05 19:46:55 +0600

    swipe listener added on update_profile page

--- app/src/main/java/com/example/appointmentsystem/views/user_profile/UserProfileActivity.java
+++ app/src/main/java/com/example/appointmentsystem/views/user_profile/UserProfileActivity.java
@@ -1,11 +1,14 @@
 package com.example.appointmentsystem.views.user_profile;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
@@ -26,6 +29,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 public class UserProfileActivity extends AppCompatActivity {
+    SwipeRefreshLayout swipeLayout;
     Button logoutButton, updateProfileButton;
     EditText fullNameET,
             serviceTypeET,
@@ -89,6 +93,97 @@ public class UserProfileActivity extends AppCompatActivity {
         }
     };
 
+    public void updateProfile() {
+        fullNameET.setError(null);
+        serviceTypeET.setError(null);
+        designationET.setError(null);
+        agencyNameET.setError(null);
+        addressET.setError(null);
+        apptPhoneET.setError(null);
+        Toast.makeText(getApplicationContext(), "Update!", Toast.LENGTH_SHORT).show();
+    }
+
+    private void setupViewComponents() {
+        swipeLayout = findViewById(R.id.swipe_profile);
+        swipeLayout.setOnRefreshListener(swipeListener);
+        logoutButton = findViewById(R.id.logoutButton);
+        fullNameTV = findViewById(R.id.full_name_field);
+        loadingPB = findViewById(R.id.profilePB);
+        updateProfileButton = findViewById(R.id.updateProfileButton);
+
+        fullNameET = findViewById(R.id.full_name_profile);
+        serviceTypeET = findViewById(R.id.service_type_profile);
+        designationET = findViewById(R.id.designation_profile);
+        agencyNameET = findViewById(R.id.agency_name_profile);
+        addressET = findViewById(R.id.address_profile);
+        addressET.setOnTouchListener(locationListener);
+        apptPhoneET = findViewById(R.id.appt_phone_profile);
+        emailET = findViewById(R.id.email_profile);
+        emailET.setEnabled(false);
+        phoneET = findViewById(R.id.phone_profile);
+        phoneET.setEnabled(false);
+
+        fbUser = FirebaseAuth.getInstance().getCurrentUser();
+        dbRef = FirebaseDatabase.getInstance().getReference();
+    }
+
+    //Page swipeRefresh listener
+    SwipeRefreshLayout.OnRefreshListener swipeListener = new SwipeRefreshLayout.OnRefreshListener() {
+        @Override
+        public void onRefresh() {
+            new DemoAsync().execute();
+        }
+    };
+
+    //Location icon tap listener
+    View.OnTouchListener locationListener = new View.OnTouchListener() {
+        @Override
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+            final int DRAWABLE_LEFT = 0;
+            final int DRAWABLE_TOP = 1;
+            final int DRAWABLE_RIGHT = 2;
+            final int DRAWABLE_BOTTOM = 3;
+
+            if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
+                if (motionEvent.getRawX() >= (addressET.getRight() - addressET.getCompoundDrawables()[DRAWABLE_RIGHT].getBounds().width())) {
+                    Toast.makeText(getApplicationContext(), "Getting location!", Toast.LENGTH_SHORT).show();
+                    return true;
+                }
+            }
+            return false;
+        }
+    };
+
+    //Update button click listener
+    View.OnClickListener updateProfileListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            hideKeyboard();
+            if (fullNameET.getText().toString().trim().isEmpty()) {
+                fullNameET.setError("Please enter your name");
+                fullNameET.requestFocus();
+            } else if (serviceTypeET.getText().toString().trim().isEmpty()) {
+                serviceTypeET.setError("Enter type of service that you offer");
+                serviceTypeET.requestFocus();
+            } else if (designationET.getText().toString().trim().isEmpty()) {
+                designationET.setError("Please enter your designation");
+                designationET.requestFocus();
+            } else if (agencyNameET.getText().toString().trim().isEmpty()) {
+                agencyNameET.setError("Please enter your company/agency name");
+                agencyNameET.requestFocus();
+            } else if (addressET.getText().toString().trim().isEmpty()) {
+                addressET.setError("Please enter your address");
+                addressET.requestFocus();
+            } else if (apptPhoneET.getText().toString().trim().isEmpty()) {
+                apptPhoneET.setError("Appointment phone number can't be empty");
+                apptPhoneET.requestFocus();
+            } else {
+                updateProfile();
+            }
+        }
+    };
+
+    //Logout button click listener
     View.OnClickListener logoutListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
@@ -101,59 +196,6 @@ public class UserProfileActivity extends AppCompatActivity {
         }
     };
 
-    View.OnClickListener updateProfileListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            hideKeyboard();
-            if (fullNameET.getText().toString().trim().isEmpty()) {
-                fullNameET.setError("Please enter your name");
-            } else if (serviceTypeET.getText().toString().trim().isEmpty()) {
-                serviceTypeET.setError("Enter type of service that you offer");
-            } else if (designationET.getText().toString().trim().isEmpty()) {
-                designationET.setError("Please enter your designation");
-            } else if (agencyNameET.getText().toString().trim().isEmpty()) {
-                agencyNameET.setError("Please enter your company/agency name");
-            } else if (addressET.getText().toString().trim().isEmpty()) {
-                addressET.setError("Please enter your address");
-            } else if (apptPhoneET.getText().toString().trim().isEmpty()) {
-                apptPhoneET.setError("Appointment phone number can't be empty");
-            } else {
-                updateProfile();
-            }
-        }
-    };
-
-    public void updateProfile() {
-        fullNameET.setError(null);
-        serviceTypeET.setError(null);
-        designationET.setError(null);
-        agencyNameET.setError(null);
-        addressET.setError(null);
-        apptPhoneET.setError(null);
-        Toast.makeText(getApplicationContext(), "Update!", Toast.LENGTH_SHORT).show();
-    }
-
-    private void setupViewComponents() {
-        logoutButton = findViewById(R.id.logoutButton);
-        fullNameTV = findViewById(R.id.full_name_field);
-        loadingPB = findViewById(R.id.profilePB);
-        updateProfileButton = findViewById(R.id.updateProfileButton);
-
-        fullNameET = findViewById(R.id.full_name_profile);
-        serviceTypeET = findViewById(R.id.service_type_profile);
-        designationET = findViewById(R.id.designation_profile);
-        agencyNameET = findViewById(R.id.agency_name_profile);
-        addressET = findViewById(R.id.address_profile);
-        apptPhoneET = findViewById(R.id.appt_phone_profile);
-        emailET = findViewById(R.id.email_profile);
-        emailET.setEnabled(false);
-        phoneET = findViewById(R.id.phone_profile);
-        phoneET.setEnabled(false);
-
-        fbUser = FirebaseAuth.getInstance().getCurrentUser();
-        dbRef = FirebaseDatabase.getInstance().getReference();
-    }
-
     private void hideKeyboard() {
         View view = getCurrentFocus();
         if (view != null) {
@@ -161,4 +203,37 @@ public class UserProfileActivity extends AppCompatActivity {
                     hideSoftInputFromWindow(view.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
         }
     }
+
+    public class DemoAsync extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+
+                e.printStackTrace();
+
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            Toast.makeText(getApplicationContext(), "Page refreshed!",
+                    Toast.LENGTH_LONG).show();
+
+            // Notify swipeRefreshLayout that the refresh has finished
+            swipeLayout.setRefreshing(false);
+        }
+
+    }
+
 }
